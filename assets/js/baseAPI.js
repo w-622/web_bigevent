1bbf8b735979fdf0f993a37153c46b7037de4ae5
itcqw <1378829824@qq.com>
2024-06-24 15:12:53 +0800

    后台主页已完成

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,3 +1,17 @@
 $.ajaxPrefiler(function(options){
     options.url = 'http://ajax.frontend.itheima.net' + options.url
+
+    if(options.url.indexOf('/my/') !== -1) {
+        options.headers =  {
+            Authorization: localStorage.getItem('token') | ''
+        }
+    }
+    
+    options.complete = function(res){
+        if(res.responseJOSN.status === 1 && res.responseJOSN.message === '身份认证失败！'){
+            localStorage.removeItem('token')
+
+            location.href = '/login.html'
+        }
+    }
 })
